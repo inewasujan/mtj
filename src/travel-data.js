c2c3a584b481534fc5bfdb213336536abec3c866
inewasujan <inewasujan@gmail.com>
2023-06-21 18:01:47 +1000

    Ver-1.1

--- src/travel-data.js
+++ src/travel-data.js
@@ -2,6 +2,15 @@
 export default [
     {
         id:1,
+        title:"Mount Everest",
+        description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point. Its elevation of 8,848.86 m was most recently established in 2020 by the Chinese and Nepali authorities",
+        imgCover: "everest.png",
+        date: "15 May, 2023 - 30 Aug, 2023",
+        location: "Nepal",
+        maplink: "https://www.google.com/maps?rlz=1C1CHBF_en-GBAU975AU975&output=search&q=mount+everest+best+time+of+the+year+to+climb&entry=mc&sa=X&ved=2ahUKEwiR-b3O79P_AhU0h1YBHaM9BNQQ0pQJegQIURAB",
+    },
+    {
+        id:2,
         title:"Mount Fuji",
         description: "Mount Fuji is the tallest mountain in Japan,standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imgCover: "mountfuji.png",
@@ -10,7 +19,7 @@ export default [
         maplink: "https://www.google.com/maps/place/Mount+Fuji/@35.3606583,138.7067638,14z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
     },
     {
-        id:2,
+        id:3,
         title:"Sydney Opera House",
         description:"The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imgCover: "sydney.png",
@@ -19,12 +28,13 @@ export default [
         maplink: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8583124,151.2060377,15z/data=!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0?entry=ttu",
     },
     {
-        id:3,
+        id:4,
         title:"Geirangerfjord",
         description:"The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imgCover: "geirangerfjord.jpg",
         date: "01 Oct, 2023 - 18 Nov, 2023",
         location: "NORWAY",
         maplink: "https://www.google.com/maps/place/Geirangerfjord/@62.1047779,6.9351325,11z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
-    }
+    },
+    
 ]
